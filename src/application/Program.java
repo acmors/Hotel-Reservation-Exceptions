8bdc97dd350ba317496bff15ea4aa5a00e9d964c
acmors <marcosantoniosilva047@gmail.com>
2024-07-25 21:44:26 -0300

    apenas alguns adicoes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,42 @@
 package application;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 import model.entities.Reservation;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat();
 
+		try {
+			System.out.println("Room number: ");
+			int number = scan.nextInt();
+			System.out.println("CheckIn Date (dd/mm/yyyy): ");
+			Date checkIn = sdf.parse(scan.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(scan.next());
+			
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+			
+			System.out.println();
+			System.out.println("Enter data to update the reservation:");
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			checkIn = sdf.parse(scan.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			checkOut = sdf.parse(scan.next());
+			
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+		}
+		
+		
+		
+		
+		scan.close();
 	}
 
 }
